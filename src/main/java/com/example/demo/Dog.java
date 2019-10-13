82ad2052905cc9951412bd1fa545672941993f81
Tony <admin@Tonys-MacBook-Pro-5.local>
2019-10-13 15:46:16 -0400

    first push

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import sun.java2d.loops.GeneralRenderer;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @Min(1)
+    private int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
